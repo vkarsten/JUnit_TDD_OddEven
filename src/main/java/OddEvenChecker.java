6961ed65594476d284232b9849ec6cb127a234a6
vkarsten <viktoria.karsten@gmail.com>
2021-12-13 14:01:52 +0100

    Add tiny bit of method documentation

--- src/main/java/OddEvenChecker.java
+++ src/main/java/OddEvenChecker.java
@@ -6,9 +6,10 @@ public class OddEvenChecker {
      * This method verifies if the given number is odder number or not.
      *
      * @param number
-     * @return
+     * @return true if number is odd, false if number is even
      */
     public boolean isOddNumber(int number) {
         return number % 2 != 0;
     }
+
 }
